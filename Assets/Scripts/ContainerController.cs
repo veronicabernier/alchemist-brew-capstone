3e7f272e488d6dab4137fef134e80e15015a1705
Veronica Bernier <veronica.bernier@upr.edu>
2023-02-22 23:53:36 -0400

    Added Bean sets to plate

---
+++ Assets/Scripts/ContainerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContainerController : MonoBehaviour
+{
+    public GameObject[] beanSets;
+    public GameObject[] beanSetsPlate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
