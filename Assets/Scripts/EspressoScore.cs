13a62cb392ee6e3386b056daaccd7de9c2c9dcb8
Veronica Bernier <veronica.bernier@upr.edu>
2023-03-06 10:50:39 -0400

    added espresso score class

---
+++ Assets/Scripts/EspressoScore.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EspressoScore
+{
+    int weightScore;
+    int weightScoreTotal;
+
+    int reservoirScore;
+    int reservoirScoreTotal;
+
+    int powerOnScore;
+    int powerOnScoreTotal;
+
+    int grindScore;
+    int grindScoreTotal;
+
+    int tampScore;
+    int tampScoreTotal;
+
+    int brewScore;
+    int brewScoreTotal;
+
+    int serveScore;
+    int serveScoreTotal;
+}
