dbfb80eda49abe6dc8173be492fa172e770c7803
Veronica Bernier <veronica.bernier@upr.edu>
2023-03-06 20:30:04 -0400

    labeled levels on inspector array

--- Assets/Scripts/SceneHandlers/EspressoHandler.cs
+++ Assets/Scripts/SceneHandlers/EspressoHandler.cs
@@ -4,39 +4,28 @@ using UnityEngine;
 
 public class EspressoHandler : MonoBehaviour
 {
-    public GameObject weighingLevelPrefab;
-    public GameObject reservoirLevelPrefab;
-    public GameObject powerOnLevelPrefab;
-    public GameObject grindLevelPrefab;
-    public GameObject tampLevelPrefab;
-    public GameObject brewLevelPrefab;
-    public GameObject serveLevelPrefab;
+    [System.Serializable]
+    public class Level
+    {
+        public string name;
+        public GameObject prefab;
+        [System.NonSerialized]
+        public GameObject instance;
+        [System.NonSerialized]
+        public int score;
+        [System.NonSerialized]
+        public int scoreTotal;
+    }
 
     private EspressoScore espressoScore;
     private int curLevel = -1;
 
-    private class Level
-    {
-        //string name;
-        public GameObject prefab;
-        public GameObject instance;
-        public int score;
-        public int scoreTotal;
-    }
-
-    private Level[] levels;
+    [LabeledArrayAttribute(new string[] { "Weighing", "Reservoir", "Power On", "Grind", "Tamp", "Brew", "Serve"})]
+    public Level[] levels = new Level[7];
 
     // Start is called before the first frame update
     void Start()
     {
-        levels = new Level[]
-        {
-            new Level
-            {
-                prefab = weighingLevelPrefab
-            }
-        };
-
         MoveLevel();
     }
 
@@ -50,6 +39,10 @@ public class EspressoHandler : MonoBehaviour
     {
         curLevel += 1;
         levels[curLevel].instance = Instantiate(levels[curLevel].prefab);
+        levels[curLevel].score = 10;
+        levels[curLevel].scoreTotal = 15;
+        //Debug.Log(espressoScore.weightScore);
+        //Debug.Log(espressoScore.weightScoreTotal);
     }
 
     void StopLevel()
