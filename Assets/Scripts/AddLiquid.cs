074e63c1640f3b4b5e4fbfff6b5bf176d3d1c453
Veronica Bernier <veronica.bernier@upr.edu>
2023-03-07 19:30:50 -0400

    generates line and determines how much water filling is correct

--- Assets/Scripts/AddLiquid.cs
+++ Assets/Scripts/AddLiquid.cs
@@ -7,18 +7,33 @@ public class AddLiquid : MonoBehaviour
 {
     public GameObject spriteMask;
     public GameObject lineGuide;
+    public float lineYMin;
+    public float lineYMax;
 
-    //public float yStart;
+    private float yStart;
     public float yEnd;
     [Tooltip("Per Second")]
     public float ySpeed = 0.001f;
 
+
+    private float correctYMask;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        yStart = spriteMask.transform.localPosition.y;
+        PositionLine();
+    }
+
+    private void PositionLine()
+    {
+        float positionPercentage = UnityEngine.Random.Range(0.00f, 1.00f);
+        float lineYPos = lineYMin + (Mathf.Abs(lineYMax - lineYMin) * positionPercentage);
+
+        lineGuide.transform.localPosition = new Vector2(lineGuide.transform.localPosition.x, lineYPos);
+
+        correctYMask = yStart + (Mathf.Abs(yEnd - yStart) * positionPercentage);
     }
 
     // Update is called once per frame
@@ -37,6 +52,8 @@ public class AddLiquid : MonoBehaviour
         {
             float newY = Mathf.Min(curY + ySpeed, yEnd);
             spriteMask.transform.localPosition = new Vector3(spriteMask.transform.localPosition.x, newY);
+            Debug.Log("curY: " + newY.ToString());
+            Debug.Log("correctY: " + correctYMask);
         }
     }
 
