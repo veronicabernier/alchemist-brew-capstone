f87af5834bbd2a8e9e3dd8e713f69d46153abcdb
Veronica Bernier <veronica.bernier@upr.edu>
2023-03-22 21:52:45 -0400

    water reservoir empties as it brews

--- Assets/Scripts/Controllers/ServeController.cs
+++ Assets/Scripts/Controllers/ServeController.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class BrewController : MonoBehaviour
+public class ServeController : MonoBehaviour
 {
 
     public GameObject progressBar;
-    public SpriteMask waterMask;
 
-    private bool filling = false;
-    private ProgressBar pb;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,17 +17,12 @@ public class BrewController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (filling)
-        {
-            waterMask.transform.localPosition = new Vector3(waterMask.transform.localPosition.x, 1 - pb.progressAmount.fillAmount, waterMask.transform.localPosition.z);
-        }
+        
     }
 
     public void ObjectPlaced()
     {
         progressBar.SetActive(true);
-        pb = progressBar.GetComponent<ProgressBar>();
-        filling = true;
     }
 
     public void ProgressDone()
