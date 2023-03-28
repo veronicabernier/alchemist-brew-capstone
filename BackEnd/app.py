605ed01f74274783648404abe5002fdec7b9c9d6
Fernando-Agosto <fernando.agosto1@upr.edu>
2023-03-28 12:27:41 -0400

    Created get_recipe and add_recipe

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -4,12 +4,26 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from BackEnd.config.dbconfig import pg_config as settings
 from flask import Flask, request
 from flask_cors import CORS
-from flask import jsonify, json
+from flask import jsonify
+import json
 
 
 app = Flask(__name__)
 #db = SQLAlchemy(app)
 CORS(app)
+
+
+def prebrew_dict(row):
+    result = {}
+    result['recipeid'] = row[0]
+    result['brew_method'] = row[1]
+    result['grind_setting'] = row[2]
+    result['brand'] = row[3]
+    result['roast'] = row[4]
+    result['bean_type'] = row[5]
+    result['coffee_weight'] = row[6]
+    result['userid'] = row[7]
+    return result
 
 def get_engine(user, passwd, host, port, db):
     url = f"postgresql://{user}:{passwd}@{host}:{port}/{db}"
@@ -50,8 +64,8 @@ engine = Session.get_bind()
 
 
 Base = declarative_base()
-class Users(Base):
-    __tablename__ = "Users"
+class users(Base):
+    __tablename__ = "users"
     userid = Column("userid", Integer, autoincrement=True, primary_key=True)
     username = Column("username",Text)
     email = Column("email", Text)
@@ -74,8 +88,8 @@ class Users(Base):
         return f"({self.username} {self.email} {self.password}" \
                f" {self.private_profile} {self.birth_date} {self.gender} {self.location})"
 
-class PreBrew(Base):
-    __tablename__ = "PreBrew"
+class recipes(Base):
+    __tablename__ = "recipes"
     recipeid = Column("recipeid", Integer, autoincrement=True, primary_key=True)
     brew_method = Column("brew_method", Text)
     grind_setting = Column("grind_setting", Integer)
@@ -95,7 +109,7 @@ class PreBrew(Base):
         self.userid = userid
 
     def __repr__(self):
-        return f"({self.brew_method} {self.grind_setting} {self.brand}" \
+        return f"({self.recipeid} {self.brew_method} {self.grind_setting} {self.brand}" \
                f" {self.roast} {self.bean_type} {self.coffee_weight} {self.userid})"
 
 Base.metadata.create_all(engine)
@@ -104,23 +118,23 @@ Base.metadata.create_all(engine)
 engine = Session.get_bind()
 
 
-@app.route("/register", methods=['POST', 'GET'])
+@app.route("/signup", methods=['POST', 'GET'])
 def register():
     if request.method == "POST":
-        username = request.args.get('username'),
-        email = request.args.get('email'),
-        password = request.args.get("password"),
-        confirm = request.args.get("confirm"),
-        birth_date = request.args.get("birth_date"),
-        gender = request.args.get("gender"),
-        location = request.args.get("location")
+        username = request.form.get('username'),
+        email = request.form.get('email'),
+        password = request.form.get("password"),
+        confirm = request.form.get("confirm"),
+        birth_date = request.form.get("birth_date"),
+        gender = request.form.get("gender"),
+        location = request.form.get("location")
 
-        emaildata = Session.query(Users).filter_by(email=email).first()
+        emaildata = Session.query(users).filter_by(email=email).first()
         print(emaildata)
         print(username, email, password, confirm, birth_date, gender, location)
         if emaildata == None:
             if password == confirm:
-                Session.add(Users(username=username, email=email, password=password, private_profile=True, birth_date=birth_date, gender=gender, location=location))
+                Session.add(users(username=username, email=email, password=password, private_profile=True, birth_date=birth_date, gender=gender, location=location))
                 Session.commit()
                 Session.flush()
                 return jsonify("Singup complete"), 201
@@ -146,8 +160,8 @@ def login():
         email = request.args.get('email')
         password = request.args.get("password")
 
-        emaildata = Session.query(Users.email).filter_by(email=email).first()
-        passworddata = Session.query(Users.password).filter_by(email=email).first()
+        emaildata = Session.query(users.email).filter_by(email=email).first()
+        passworddata = Session.query(users.password).filter_by(email=email).first()
 
         print(emaildata)
         print(passworddata)
@@ -164,14 +178,41 @@ def login():
 
     return jsonify(Error="User or password is incorrect"), 400
 
-# @app.route("/email/recipe list", methods=["POST", "GET"])
-# def prebrew():
-#     result_list = []
-#     if request.method == "GET":
-#         userid = request.args.get('userid')
-#         recipes = Session.query(PreBrew).filter_by(userid=userid).all()
-#         print(recipes)
-#         return json.dumps(recipes), 200
+def encoder_recipes(recipe):
+    if isinstance(recipe, recipes):
+        return {'recipeid': recipe.recipeid, 'brew_method': recipe.brew_method, 'grind_setting': recipe.grind_setting, 'brand': recipe.brand,
+            'roast': recipe.roast, 'bean_type': recipe.bean_type, 'coffee_weight': recipe.coffee_weight, 'userid': recipe.userid}
+    raise TypeError(f'Object{recipe} is not of type recipes.')
+
+@app.route("/email/recipe list", methods=["POST", "GET"])
+def get_recipes():
+    result_list = []
+    if request.method == "GET":
+        userid = request.form.get('userid')
+        prebrew = Session.query(recipes).filter_by(userid=userid).all()
+
+        for i in prebrew:
+            result=encoder_recipes(i)
+            result_list.append(result)
+        return jsonify(Recepies=result_list), 200
+
+@app.route("/email/recipe list/add", methods=["POST", "GET"])
+def add_recipe():
+    if request.method == "POST":
+        brew_method = request.form.get('brew_method')
+        grind_setting = request.form.get('grind_setting')
+        brand = request.form.get('brand')
+        roast = request.form.get('roast')
+        bean_type = request.form.get('bean_type')
+        coffee_weight = request.form.get('coffee_weight')
+        userid = request.form.get('userid')
+
+        Session.add(
+            recipes(brew_method=brew_method, grind_setting=grind_setting, brand=brand, roast=roast,
+                  bean_type=bean_type, coffee_weight=coffee_weight, userid=userid, ))
+        Session.commit()
+        Session.flush()
+        return jsonify("Recipe created"), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
