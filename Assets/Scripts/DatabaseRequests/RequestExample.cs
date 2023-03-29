c3505b87cb63f9cd3bead059a65333a5fed721ff
Veronica Bernier <veronica.bernier@upr.edu>
2023-03-28 21:51:29 -0400

    database connection example

---
+++ Assets/Scripts/DatabaseRequests/RequestExample.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class RequestExample : MonoBehaviour
+{
+
+    void Start()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("username", "Rey");
+        form.AddField("email", "reinaldo2@upr.edu");
+        form.AddField("password", "888");
+        form.AddField("confirm", "888");
+        form.AddField("private_profile", "True");
+        form.AddField("birth_date", "1998-02-03");
+        form.AddField("gender", "Male");
+        form.AddField("location", "Puerto Rico");
+
+        StartCoroutine(PostRequest("http://127.0.0.1:5000/signup", form));
+    }
+
+    IEnumerator PostRequest(string uri, WWWForm postData)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Post(uri, postData))
+        {
+            yield return webRequest.SendWebRequest();
+            if (webRequest.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.Log("Error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log(webRequest.downloadHandler.text);
+            }
+        }
+    }
+
+}
