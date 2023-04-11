ee4663a14c297ed8c368fe1284e1fb1e9712976b
Veronica Bernier <veronica.bernier@upr.edu>
2023-04-11 11:31:54 -0400

    scores classes

---
+++ Assets/Scripts/Classes/ChemexScore.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChemexScore
+{
+    public int weightScore;
+    public int weightScoreTotal;
+
+    public int grindScore;
+    public int grindScoreTotal;
+
+    public int chooseWaterScore;
+    public int chooseWaterScoreTotal;
+
+    public int addCoffeeScore;
+    public int addCoffeeScoreTotal;
+
+    public int putTogetherScore;
+    public int putTogetherTotal;
+
+    public int stoveScore;
+    public int stoveScoreTotal;
+
+    public int serveScore;
+    public int serveScoreTotal;
+}
---
+++ Assets/Scripts/Classes/MokapotScore.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MokapotScore
+{
+    public int weightScore;
+    public int weightScoreTotal;
+
+    public int grindScore;
+    public int grindScoreTotal;
+
+    public int wetGroundsScore;
+    public int wetGroundsScoreTotal;
+
+    public int addWaterScore;
+    public int addWaterScoreTotal;
+
+    public int serveScore;
+    public int serveScoreTotal;
+}
