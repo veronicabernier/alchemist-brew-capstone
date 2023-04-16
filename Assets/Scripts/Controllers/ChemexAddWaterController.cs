f61284a1dc4ac31b489607fe95cbccbd55bdb1dc
Veronica Bernier <veronica.bernier@upr.edu>
2023-04-15 22:29:05 -0400

    chemex add water IMPROVED

--- Assets/Scripts/Controllers/ChemexAddWaterController.cs
+++ Assets/Scripts/Controllers/ChemexAddWaterController.cs
@@ -6,19 +6,28 @@ using UnityEngine;
 
 public class ChemexAddWaterController : MonoBehaviour
 {
-    //1. generate line pos y
-    //2. fill water
-    //3. filter
 
-    public GameObject chemex;
+    public MovementBar mb;
 
     public GameObject waterDoneButton;
 
     public TextMeshProUGUI prompt;
     public GameObject progressBar;
 
+    public SpriteMask waterMask;
+    public float waterMaskMin;
+    public float waterMaskMax;
+    public SpriteRenderer placedGrounds;
+
+    public SpriteMask coffeeMask;
+    public float coffeeMaskMin;
+    public float coffeeMaskMax;
+
     public GameObject timer;
 
+    private ProgressBar pb;
+    private float waterMaskReached;
+    private float coffeeMaskReached;
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +38,22 @@ public class ChemexAddWaterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (progressBar.activeSelf)
+        {
+            //filtering
+            float newYPos = waterMaskMin + (1 - pb.progressAmount.fillAmount) * Math.Abs(waterMaskReached - waterMaskMin);
+            waterMask.transform.localPosition = new Vector3(waterMask.transform.localPosition.x, newYPos, waterMask.transform.localPosition.z);
+
+            newYPos = coffeeMaskMin + pb.progressAmount.fillAmount * Math.Abs(coffeeMaskReached - coffeeMaskMin);
+            coffeeMask.transform.localPosition = new Vector3(coffeeMask.transform.localPosition.x, newYPos, coffeeMask.transform.localPosition.z);
+
+        }
+        else
+        {
+            //adding water
+            float newYPos = waterMaskMin + mb.fillProgress.fillAmount * Math.Abs(waterMaskMax - waterMaskMin);
+            waterMask.transform.localPosition = new Vector3(waterMask.transform.localPosition.x, newYPos, waterMask.transform.localPosition.z);
+        }
 
     }
 
@@ -36,20 +61,18 @@ public class ChemexAddWaterController : MonoBehaviour
     {
         prompt.text = "Wait!";
         waterDoneButton.SetActive(false);
-        chemex.GetComponent<AddLiquid>().enabled = false;
         progressBar.SetActive(true);
+        pb = progressBar.GetComponent<ProgressBar>();
+        placedGrounds.maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
+        waterMaskReached = waterMask.transform.localPosition.y;
+        coffeeMaskReached = coffeeMaskMin + Math.Abs(coffeeMaskMax-coffeeMaskMin) * (1-(Math.Abs(waterMaskReached - waterMaskMax) / Math.Abs(waterMaskMax - waterMaskMin)));
 
         timer.SetActive(false);
     }
 
-
     public void ProgressDone()
     {
-        this.SendMessageUpwards("StopLevel", chemex.GetComponent<AddLiquid>().GetCurrentScore());
+        mb.LevelFinished();
     }
 
-    public void LevelFinished()
-    {
-        ProgressDone();
-    }
 }
